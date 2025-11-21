6e767df039ff7adb6465df719cfbb14bcbf3890e
muhammad saqib <tiktokusr4300@gmail.com>
2025-11-21 13:27:47 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "package.json",
-      "use": "@vercel/static-build",
-      "config": {
-        "distDir": "dist"
-      }
-    }
-  ],
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "/index.html"
-    }
-  ]
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
+})
